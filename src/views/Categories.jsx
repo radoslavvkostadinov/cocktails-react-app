e6257d1905ce33eefbe135a8ad859b3f8954d793
Radoslav Kostadinov <bounslay@gmail.com>
2024-09-13 16:46:26 +0300

    Add pagination to categories

--- src/views/Categories.jsx
+++ src/views/Categories.jsx
@@ -1,4 +1,5 @@
 import CardItem from "@/components/Card/CardItem";
+import Paginated from "@/components/Pagination/Pagination";
 import SelectCategory from "@/components/SelectCategory/SelectCategory";
 import { useDrinksStore } from "@/store/drinksStore";
 import { useEffect, useState } from "react";
@@ -6,6 +7,8 @@ import { useEffect, useState } from "react";
 export default function Categories() {
 
     const { drinks, loading, error, fetchDrinks } = useDrinksStore();
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 15;
     const [chooseCategory, setChooseCategory] = useState('Ordinary_Drink');
 
     useEffect(() => {
@@ -18,22 +21,33 @@ export default function Categories() {
 
         const drink = e.target.textContent === 'Ordinary Drinks' ?
             'Ordinary_Drink' : e.target.textContent
-
         setChooseCategory(drink);
+        setCurrentPage(1);
 
     };
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
+
+    const totalPages = Math.ceil(drinks.length / itemsPerPage);
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const currentItems = drinks.slice(startIndex, endIndex);
     return (
         <>
             <SelectCategory onClick={handleCategoryChange} />
 
             {chooseCategory && (
-                <div className="bg-indigo-950 pt-5">
-                    {drinks && drinks.length > 0 ? (
-                        <div className="grid grid-cols-5 gap-5 m-5">
-                            {drinks.map((drink) => (
+                <div className="bg-indigo-950 pt-5 pb-4">
+                    {currentItems && currentItems.length > 0 ? (
+                        <div className="grid grid-cols-5 gap-5 m-5 h-full">
+                            {currentItems.map((drink) => (
                                 <CardItem
                                     key={drink.idDrink}
                                     id={drink.idDrink}
@@ -42,12 +56,17 @@ export default function Categories() {
                                 />
                             ))}
                         </div>
-
                     ) : (
                         <p>No drinks found.</p>
                     )}
                 </div>
+
             )}
+            <Paginated
+                totalPages={totalPages}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+            />
         </>
     );
 }
